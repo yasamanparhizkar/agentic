1433afb2756a07145968e1990cc8955c9608b37d
Yasaman Parhizkar <yasaman.parhizkar@gmail.com>
2025-05-19 13:00:12 -0400

    simple agent with search tool

---
+++ langchain/simple_agent.py
@@ -0,0 +1,38 @@
+import os
+from langchain_google_genai  import ChatGoogleGenerativeAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load environment variables
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+
+# Create the agent
+memory = MemorySaver()
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, verbose=True)
+search = TavilySearchResults(max_results=2)
+tools = [search]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+# Use the agent
+config = {"configurable": {"thread_id": "abc123"}}
+for step in agent_executor.stream(
+    {"messages": [HumanMessage(content="hi im bob! and i live in sf")]},
+    config,
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
+
+
+for step in agent_executor.stream(
+    {"messages": [HumanMessage(content="whats the weather where I live?")]},
+    config,
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
+
+
